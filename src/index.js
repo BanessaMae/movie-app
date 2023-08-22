747352bc4381f29ed892034bba7bbd4430a7cc04
Banessa Mikhaylova <maebanessa@gmail.com>
2023-08-22 22:15:52 +0900

    1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 
-import App from './services/components/app';
-import '../src/index.css';
+import App from '../src/components/app/app';
+import './index.css';
 
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
